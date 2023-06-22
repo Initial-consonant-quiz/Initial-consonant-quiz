8c0659a0205be3c7e92d84c45ef789924e1e8b1c
howon-ryu <qorgh2akfl@naver.com>
2023-06-22 18:50:20 +0900

    howon

--- static/js/index.js
+++ static/js/index.js
@@ -45,17 +45,47 @@ socket.on('update', function(data) {
 function send() {
   // 입력되어있는 데이터 가져오기
   var message = document.getElementById('test').value
+  var answer_consonant = document.getElementById('question').value
   // 가져왔으니 데이터 빈칸으로 변경
   document.getElementById('test').value = ''
-
+  
+  let answer = sumInitial(message)
+  
   // 내가 전송할 메시지 클라이언트에게 표시
-  var chat = document.getElementById('chat')
+  
+
+  // 서버로 message 이벤트 전달 + 데이터와 함께
+  if(answer_consonant == answer){
+    var chat = document.getElementById('chat')
   var msg = document.createElement('div')
   var node = document.createTextNode(message)
   msg.classList.add('me')
   msg.appendChild(node)
   chat.appendChild(msg)
+    socket.emit('message', {type: 'message', message: message})
+  }else{
+    alert("초성을 잘못 입력하셨습니다")
+  }
+  
+}
 
-  // 서버로 message 이벤트 전달 + 데이터와 함께
-  socket.emit('message', {type: 'message', message: message})
+
+function initial(word) { // 한 글자 초성
+  const initWord = [
+     'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ',
+        'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ',
+        'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
+  ];
+  const START_UNICODE = 44032;
+  const wordUnicocde = word.charCodeAt(0);
+  const word_size = wordUnicocde - START_UNICODE;
+  const initial_idx = parseInt(word_size / 588);
+  return initWord[initial_idx];
+};
+
+function sumInitial(word) { // 두 글자로 나눠 초성 더하기
+  const stWord = word[0];
+  const lsWord = word[1];
+  
+  return initial(stWord) + initial(lsWord);
 }
